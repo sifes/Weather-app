3675980a09ac9af02bbb6dee0808943d58fdccce
sifes <lonfark@gmail.com>
2023-12-01 11:36:49 +0200

    fix: @babel/plugin-proposal-private-property-in-object

--- src/router/Layout.tsx
+++ src/router/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { Header } from '../components/shared/header';
+import { Header } from '../components/shared/header/Header';
 
 const Layout: React.FC = () => {
   return (
